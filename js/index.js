140be1e3526e994dc6cad4fb6037a03ad974c0d8
DTIGitOne <dammirtaljanovic@gmail.com>
2023-11-19 17:30:00 +0100

    Prva verzija

---
+++ js/index.js
@@ -0,0 +1,24 @@
+console.log("Damir Taljanovic");
+console.warn("Zovem se Damir");
+console.error("0204004170045");
+
+const imePrezime="Damir Taljanovic";
+const biografija="Zovem se Damir";
+const JMBG="0204004170045";
+
+console.log(imePrezime);
+console.warn(biografija);
+console.error(JMBG);
+
+let user = {
+   nameSurname:"Damir Taljanovic",
+   biography:"Zovem se Damir",
+   JMBG1:"204004170045",
+}
+
+user.nameSurname="DamirTaljanovic1";
+//zoboravio dodati neki novi tekst
+
+console.log(user.nameSurname);
+console.warn(user.biography);
+console.error(user.JMBG1);
